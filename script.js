56bd443c145447c38c2f561e9263c022d7a72270
Revan-afk-end <revanantoni112@gmail.com>
2025-08-28 21:52:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function rate(){
+  document.getElementById("rate1").innerHTML="elah";
+}
+
+function rate2(){
+  document.getElementById("rate2").innerHTML="masa 2 cuk";
+}
+
+function rate3(){
+  document.getElementById("rate3").innerHTML="serius dikit";
+}
+
+function rate4(){
+  document.getElementById("rate4").innerHTML="ah";
+}
+
+function rate5(){
+  document.getElementById("rate5").innerHTML="setengah2 ngga baik";
+}
+
+function rate6(){
+  document.getElementById("rate6").innerHTML="dikit lagi";
+}
+
+function rate7(){
+  document.getElementById("rate7").innerHTML="dikit lagi bang";
+}
+
+function rate8(){
+  document.getElementById("rate8").innerHTML="nice";
+}
+
+function rate9(){
+  document.getElementById("rate9").innerHTML="nice team";
+}
+
+function rate10(){
+  document.getElementById("rate10").innerHTML="tengkyu lek";
+}
+
